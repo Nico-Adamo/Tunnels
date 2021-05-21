99eaf005f7e46f7bf09cff55fbe4c4fadd771a11
Bilge Gungoren <bgungore@caltech.edu>
2021-05-20 21:27:02 -0700

    Player health implemented (needs fixing for flipped heart)

--- demo/tilemap_demo.c
+++ demo/tilemap_demo.c
@@ -19,6 +19,9 @@
 
 const char *SPRITE = "knight";
 const char *SPRITE_PATH = "assets/knight_f_idle_anim_f0.png";
+const char *FULL_HEART = "assets/ui/ui_heart_full.png";
+const char *HALF_HEART = "assets/ui/ui_heart_half.png";
+const char *EMPTY_HEART = "assets/ui/ui_heart_empty.png";
 
 
 const double MAX_WIDTH = 1024;
@@ -34,6 +37,10 @@ const vector_t PADDLE_UP_VELOCITY = {
     .y = 300
 };
 
+const double HALF_HEART_HEALTH = 5;
+const double PLAYER_HEALTH = 100;
+const double HEART_PADDING = 4;
+
 
 body_t *make_demo_sprite(double x, double y, char *type, sprite_info_t info) {
     SDL_Texture *texture = sdl_load_texture(SPRITE_PATH);
@@ -45,6 +52,24 @@ body_t *make_demo_sprite(double x, double y, char *type, sprite_info_t info) {
     // First argument: Sprite size (x,y are always 0)
     // Second argument: Bottom left corner of sprite, and size (we should eventually change to scale factor rather than specifying explicit width and height)
     return body_init_with_info(body_shape, texture, 100, 4, type, info);
+}
+
+UI_t *make_demo_heart(double x, double y, char *texture_name, char *type) {
+    SDL_Texture *texture = sdl_load_texture(texture_name);
+    SDL_Rect shape = (SDL_Rect) {0, 0, 16, 16};
+    rect_t hitbox = (rect_t) {x, y, 32, 32};
+    return UI_init(shape, hitbox, texture, type, 2);
+}
+
+list_t *get_player_hearts(scene_t *scene) {
+    list_t *UIs = scene_get_UI_components(scene);
+    list_t *hearts = list_init(list_size(UIs), NULL);
+    for (size_t i = 0; i < list_size(UIs); i++) {
+        if (strcmp(UI_get_type(list_get(UIs, i)), "PLAYER_HEART") == 0) {
+            list_add(hearts, list_get(UIs, i));
+        }
+    }
+    return hearts;
 }
 
 scene_t *scene_reset() {
@@ -65,6 +90,13 @@ scene_t *scene_reset() {
         scene_add_body(scene, temp_enemy);
     }
 
+    // Initialize Demo Heart
+    int heart_num = PLAYER_HEALTH / (HALF_HEART_HEALTH * 2);
+    for (int i = 0; i < heart_num; i++) {
+        UI_t *heart = make_demo_heart(HEART_PADDING + 32 * i, MAX_HEIGHT - 32 - HEART_PADDING, FULL_HEART, "PLAYER_HEART");
+        scene_add_UI_component(scene, heart);
+    }
+
     // Create Scene
 
     return scene;
@@ -78,7 +110,7 @@ int main(int arg_c, char *arg_v[]) {
     sprite_info_t player_info = {
         .experience = 0,
         .attack = 5,
-        .health = 50,
+        .health = PLAYER_HEALTH,
         .cooldown = 0
     };
 
@@ -93,6 +125,8 @@ int main(int arg_c, char *arg_v[]) {
     map_load(game, "assets/levels/map_bigger.map", 20, 20);
     double seconds = 0;
     create_tile_collision(game_get_current_scene(game), game_get_player(game));
+    SDL_Texture *half_heart = sdl_load_texture(HALF_HEART);
+    SDL_Texture *empty_heart = sdl_load_texture(EMPTY_HEART);
 
     sdl_on_key(on_key);
     while(!sdl_is_done(game)) {
@@ -119,6 +153,28 @@ int main(int arg_c, char *arg_v[]) {
             create_tile_collision(game_get_current_scene(game), game_get_player(game));
 
         }
+        
+        // Player heart adjustment
+        // Deal with fractions later
+        double lost_player_halves = (PLAYER_HEALTH - body_get_sprite_info(player).health) / HALF_HEART_HEALTH;
+        int full_hearts_lost = floor(lost_player_halves / 2);
+        bool half_heart_lost = false;
+        if (round(lost_player_halves) > full_hearts_lost * 2) {
+            half_heart_lost = true;
+        }
+
+        list_t *hearts = get_player_hearts(scene);
+        for (size_t i = list_size(hearts) - 1; i > list_size(hearts) - 1 - full_hearts_lost; i--) {
+            UI_set_texture(list_get(hearts, i), empty_heart);
+        }
+        if (half_heart_lost) {
+            size_t idx = list_size(hearts) - 1 - full_hearts_lost;
+            UI_set_texture(list_get(hearts, idx), half_heart);
+        }
+
+
+
+
 
     }
     game_free(game);
